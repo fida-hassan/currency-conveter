44c00eea6ca14b4e6e81a711c2ebbb975d52d987
Your Name <you@example.com>
2024-04-27 01:20:39 -0700

    created currency converter

---
+++ currency.ts
@@ -0,0 +1,50 @@
+import inquirer from "inquirer"
+
+
+
+const currency: any ={
+    USD: 1, //base currency
+    EUR: 0.91,
+    GBP: 0.79,
+    INR: 74.57,
+    PKR: 280,
+
+};
+
+let user_answers = await inquirer.prompt([
+    
+    {
+        name: 'from',
+        message:"enter from currency",
+        type: 'list',
+        choices: ['USD','EUR', 'GBP', 'INR', 'PKR']
+      },
+
+      {
+        name: "to",
+        message: "enter to currency",
+        type: 'list',
+        choices: ['USD','EUR', 'GBP', 'INR', 'PKR']
+      },
+
+
+      {
+        name: 'amount',
+        message:"enter your amount currency",
+        type: 'number'
+       
+      }
+
+]
+)
+
+
+let fromAmount = currency[user_answers.from]
+let toAmount = currency[user_answers.to]
+let amount = user_answers.amount
+let baseAmount = amount / fromAmount // USD
+let convertedAmount =baseAmount * toAmount
+console.log(convertedAmount);
+// console.log(fromAmount);
+// console.log(toAmount);
+// console.log(amount);
